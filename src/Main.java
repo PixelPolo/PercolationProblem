270e88e14ecae7ae37f2fee3f43aa5b7be67027b
PixelPolo <paulr237@gmail.com>
2023-10-29 18:31:08 +0100

    Percolation Problem

--- src/Main.java
+++ src/Main.java
@@ -12,29 +12,28 @@ public class Main {
     public static void main(String[] args) {
         int schemaSize = 100;
         int schemaQuantity = 100; // For each vacancyP from 0.000 to 1 (1000 times)
-        testSchemas(schemaQuantity, schemaSize, new WUPercolation(), "Weight Union");
-        testSchemas(schemaQuantity, schemaSize, new QUPercolation(), "Quick Union");
         testSchemas(schemaQuantity, schemaSize, new QFPercolation(), "Quick Find");
+        testSchemas(schemaQuantity, schemaSize, new QUPercolation(), "Quick Union");
+        testSchemas(schemaQuantity, schemaSize, new WUPercolation(), "Weight Union");
     }
+
 
     private static void testSchemas(int schemaQuantity, int schemaSize, AUFPercolation algo, String algoTitle) {
         // Swing Plot
         IQueue<Double> percolations = new LinkedListQueue<>();
         IQueue<Double> vacancyProbability = new LinkedListQueue<>();
-
         // Processing
         Schema s;
         int percolationTracker;
         long schemaAnalysedTracker = 0L;
         double elapsedTime = 0;
         long startTime = System.currentTimeMillis();
-
         for (double p = 0.000; p <= 1.000; p += 0.001) {
             percolationTracker = 0;
             for (int i = 0; i < schemaQuantity; i++) {
                 ++schemaAnalysedTracker;
                 s = new Schema(p, schemaSize);
-                algo.processSchema(s, true);
+                algo.processSchema(s, true); // true because we will not reuse the schema
                 if (algo.percolates()) percolationTracker++;
                 elapsedTime = (System.currentTimeMillis() - startTime) / 1000.;
                 System.out.printf("\rSchema to process : %,d / %,d     time : %.3f seconds",
@@ -44,12 +43,13 @@ public class Main {
             percolations.enqueue((double) percolationTracker / schemaQuantity * 100);
             vacancyProbability.enqueue(p * 100);
         }
-
         // Swing plot
         String title = String.format("%s : %s schemas of size %,d in %.3f seconds",
                 algoTitle, schemaQuantity * 1000, schemaSize, elapsedTime);
         new SwingPlot(title, 5, vacancyProbability, percolations);
     }
+
+
 
 
 }
